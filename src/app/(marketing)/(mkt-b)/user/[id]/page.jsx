c27af8661f3f5f07e6881f2b117ca170fa67100f
hamskid-id <lawalhamzat27@gmail.com>
2025-05-04 21:29:49 +0100

    updated

--- src/app/(marketing)/(mkt-b)/user/[id]/page.jsx
+++ src/app/(marketing)/(mkt-b)/user/[id]/page.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { UserAccount } from '@/components/sections/userProfile/account/UserAccount'
 import React from 'react'
 
@@ -7,13 +6,3 @@ const UserAccountPage = () => {
 }
 
 export default UserAccountPage
-=======
-import { UserAccount } from '@/components/sections/userProfile/UserAccount'
-import React from 'react'
-
-const UserAccountPage = () => {
-  return <UserAccount/>
-}
-
-export default UserAccountPage
->>>>>>> 1b87c3149483b589248f01529de3f84f691583ce
--- src/app/(marketing)/(mkt-b)/user/page.jsx
+++ src/app/(marketing)/(mkt-b)/user/page.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { UserProfile } from '@/components/sections/userProfile/profile'
-=======
-import { UserProfile } from '@/components/sections/userProfile'
->>>>>>> 1b87c3149483b589248f01529de3f84f691583ce
 import React from 'react'
 
 const UsersProfile = () => {
