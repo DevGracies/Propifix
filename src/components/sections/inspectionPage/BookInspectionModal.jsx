9824d11dca4e0a80aae34970240b40d1696339b8
Aweroro Hadi <hadistic20@gmail.com>
2025-06-18 12:50:11 +0100

    refactor: updated the inspection page to be responsive

--- src/components/sections/inspectionPage/BookInspectionModal.jsx
+++ src/components/sections/inspectionPage/BookInspectionModal.jsx
@@ -1,14 +1,18 @@
 'use client'
 
-import React, { useState } from 'react'
+import React from 'react'
 import Link from 'next/link'
 import { ModalWrapper } from '@/components/custom-ui/Modal'
 import Image from 'next/image'
 import { BedDouble, Bath, Toilet } from 'lucide-react'
+import { format } from 'date-fns'
 
-const BookInspectionModal = () => {
-  const [open, setOpen] = useState(false)
-
+const BookInspectionModal = ({
+  open,
+  setOpen,
+  selectedDate,
+  selectedTime,
+}) => {
   return (
     <ModalWrapper
       open={open}
@@ -16,20 +20,22 @@ const BookInspectionModal = () => {
       trigger={
         <button
           onClick={() => setOpen(true)}
-           className='w-full bg-white text-[#5D14AD] hover:bg-gradient-to-r hover:from-[#5D14AD] hover:to-[#9747FF] hover:text-white transition-all duration-300 px-6 py-3 rounded-lg font-semibold'>
+          className="w-full bg-white text-[#5D14AD] hover:bg-gradient-to-r hover:from-[#5D14AD] hover:to-[#9747FF] hover:text-white transition-all duration-300 text-sm md:text-base px-4 py-2 md:px-6 md:py-3 rounded-lg font-semibold"
+        >
           Book for Inspection
         </button>
       }
-      bigscreenwidth="max-w-xl w-full"
+      bigscreenwidth="w-full max-w-[90vw] sm:max-w-xl"
     >
-      <div className="space-y-6 p-4 md:p-6">
-        <h2 className="text-center text-xl md:text-2xl font-semibold text-[#5D14AD]">
-          Go Tour this Property on Sunday, February 16
+      <div className="space-y-6 p-4 sm:p-6">
+        <h2 className="text-center text-lg md:text-2xl font-semibold text-[#5D14AD]">
+          Go Tour this Property on{' '}
+          {format(new Date(selectedDate), 'EEEE, MMMM d')} | {selectedTime}
         </h2>
 
-        {/* Property Info */}
-        <div className="flex items-start gap-4">
-          <div className="w-28 h-24 relative flex-shrink-0 rounded-md overflow-hidden">
+        {/* Property Summary */}
+        <div className="flex flex-col sm:flex-row items-start sm:items-center gap-4">
+          <div className="w-full sm:w-28 h-40 sm:h-24 relative flex-shrink-0 rounded-md overflow-hidden">
             <Image
               src="/images/inspection-house.png"
               alt="Property"
@@ -38,16 +44,16 @@ const BookInspectionModal = () => {
             />
           </div>
 
-          <div className="space-y-1">
-            <h3 className="text-xl font-bold text-black">₦ 450,000</h3>
-            <div className="flex items-center gap-4 text-sm text-black/80 font-semibold">
-              <span className="flex text-nowrap items-center gap-1">
+          <div className="space-y-1 w-full">
+            <h3 className="text-lg md:text-xl font-bold text-black">₦ 450,000</h3>
+            <div className="flex flex-wrap items-center gap-x-4 gap-y-1 text-sm text-black/80 font-semibold">
+              <span className="flex items-center gap-1 whitespace-nowrap">
                 <BedDouble size={16} /> 6 Bedrooms
               </span>
-              <span className="flex text-nowrap items-center gap-1">
+              <span className="flex items-center gap-1 whitespace-nowrap">
                 <Bath size={16} /> 6 Bathrooms
               </span>
-              <span className="flex text-nowrap items-center gap-1">
+              <span className="flex items-center gap-1 whitespace-nowrap">
                 <Toilet size={16} /> 4 Toilets
               </span>
             </div>
@@ -60,21 +66,21 @@ const BookInspectionModal = () => {
           <input
             type="text"
             placeholder="Enter your full name"
-            className="w-full px-4 py-3 border italic border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 placeholder:text-black/80"
+            className="w-full px-4 py-3 placeholder:text-sm md:placeholder:text-base border italic border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 placeholder:text-black/80"
           />
           <input
             type="email"
             placeholder="Enter your email address"
-            className="w-full px-4 py-3 border italic border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 placeholder:text-black/80"
+            className="w-full px-4 py-3 placeholder:text-sm md:placeholder:text-base border italic border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 placeholder:text-black/80"
           />
-          <div className="flex gap-4">
-            <span className="px-4 py-3 border border-gray-300 rounded-md text-black/80">
+          <div className="flex flex-col md:flex-row gap-4">
+            <span className="px-4 py-3 placeholder:text-sm md:placeholder:text-base border border-gray-300 rounded-md text-black/80">
               +234
             </span>
             <input
               type="tel"
               placeholder="Enter your phone number"
-              className="w-full px-4 py-3 border italic border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 placeholder:text-black/80"
+              className="w-full px-4 py-3 placeholder:text-sm md:placeholder:text-base border italic border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 placeholder:text-black/80"
             />
           </div>
         </div>
@@ -84,18 +90,15 @@ const BookInspectionModal = () => {
           PropiFix will call you within the next business hour to match you with a
           PropiFix™ network agent. By submitting this information, I acknowledge that
           I have read and agreed to the{' '}
-          <span className="underline font-medium cursor-pointer text-black">
-            Terms of use
-          </span>{' '}
+          <span className="underline font-medium cursor-pointer text-black">Terms of use</span>{' '}
           and{' '}
-          <span className="underline font-medium cursor-pointer text-black">
-            Privacy Policy
-          </span>
-          .
+          <span className="underline font-medium cursor-pointer text-black">Privacy Policy</span>.
         </p>
 
-        <Link href={'/inspection-payment'} >
-          <button className="w-full cursor-pointer bg-gradient-to-r from-[#5D14AD] to-[#9747FF] text-white py-3 rounded-md font-semibold hover:opacity-90 transition">Book for Inspection now</button>
+        <Link href="/inspection-payment">
+          <button className="w-full cursor-pointer bg-gradient-to-r from-[#5D14AD] to-[#9747FF] text-white py-3 rounded-md font-semibold hover:opacity-90 transition">
+            Book for Inspection now
+          </button>
         </Link>
       </div>
     </ModalWrapper>
--- src/components/sections/inspectionPage/index.jsx
+++ src/components/sections/inspectionPage/index.jsx
@@ -21,14 +21,15 @@ const Inspection = () => {
     { label: 'Evening', time: '4pm - 8pm' }
   ]
 
+  const images = Array(12).fill('/images/inspection-house.svg')
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0)
   const [currentIndex, setCurrentIndex] = useState(0)
   const [selectedDate, setSelectedDate] = useState(dates[0].date)
   const [selectedTime, setSelectedTime] = useState('Morning')
-
-  const selectedTimeSlot = timeSlots.find(slot => slot.label === selectedTime)?.time || ''
-
   const [showModal, setShowModal] = useState(false)
 
+  const selectedTimeSlot = timeSlots.find(slot => slot.label === selectedTime)?.time || ''
 
   const handlePrev = () => {
     if (currentIndex > 0) {
@@ -46,36 +47,47 @@ const Inspection = () => {
     }
   }
 
+  const handleImagePrev = () => {
+    setCurrentImageIndex(prev =>
+      prev === 0 ? images.length - 1 : prev - 1
+    )
+  }
+
+  const handleImageNext = () => {
+    setCurrentImageIndex(prev =>
+      prev === images.length - 1 ? 0 : prev + 1
+    )
+  }
+
   return (
     <div className='p-6 md:p-10 mt-20 space-y-10'>
       <BackButton />
-      <h1 className='text-2xl font-semibold text-[#9D71C6]'>
+      <h1 className='text-lg md:text-2xl font-semibold text-[#9D71C6]'>
         Book <span className='text-[#5D14AD]'>Inspection</span>
       </h1>
 
       <div className='flex flex-col md:flex-row gap-10 items-stretch'>
         <div className='w-full md:w-3/5 relative rounded-xl overflow-hidden flex-shrink-0'>
           <Image
-            src='/images/inspection-house.png'
-            alt='Property'
+            src={images[currentImageIndex]}
+            alt={`Property ${currentImageIndex + 1}`}
             width={800}
             height={600}
-            className='w-full h-full object-cover rounded-xl'
+            className='w-full h-full object-cover rounded-xl transition-all duration-300 ease-in-out'
           />
           <div className='absolute inset-y-0 left-0 flex items-center px-2'>
-            <button className='bg-white text-black p-2 rounded-full'>
+            <button className='bg-white text-black p-2 rounded-full' onClick={handleImagePrev}>
               <ChevronLeft size={20} />
             </button>
           </div>
           <div className='absolute inset-y-0 right-0 flex items-center px-2'>
-            <button className='bg-white text-black p-2 rounded-full'>
+            <button className='bg-white text-black p-2 rounded-full' onClick={handleImageNext}>
               <ChevronRight size={20} />
             </button>
           </div>
-
-          <div className='absolute bottom-0 left-1/2 -translate-x-1/2 bg-gray-200 px-6 py-2 rounded-full rounded-b-none flex items-center gap-2 shadow-md'>
-            <Images />
-            1/12
+          <div className='absolute bottom-0 left-1/2 -translate-x-1/2 bg-gray-200 px-2 py-1 md:px-6 md:py-2 text-xs md:text-base rounded-full rounded-b-none flex items-center gap-2 shadow-md'>
+            <Images width={20} />
+            {currentImageIndex + 1}/{images.length}
           </div>
         </div>
 
@@ -84,11 +96,8 @@ const Inspection = () => {
           <div>
             <h2 className='font-semibold text-center text-lg md:text-2xl mb-4'>Date</h2>
             <div className='flex items-center gap-4 overflow-x-auto scrollbar-hide'>
-              <button
-                onClick={handlePrev}
-              >
-                {/* <ChevronLeft size={20} /> */}
-                <Image src={'/icons/left-arrow-button.svg'} alt='button' width={200} height={20}/>
+              <button onClick={handlePrev}>
+                <Image src={'/icons/left-arrow-button.svg'} alt='button' width={200} height={20} />
               </button>
               {dates.map((d, index) => (
                 <div
@@ -103,15 +112,13 @@ const Inspection = () => {
                       : 'border-gray-200'
                   }`}
                 >
-                  <p className='text-sm font-semibold'>{d.label}</p>
-                  <p className='text-xl font-bold'>{d.short}</p>
-                  <p className='text-sm'>{d.month}</p>
+                  <p className='text-xs md:text-sm font-semibold'>{d.label}</p>
+                  <p className='text-xs md:text-xl font-bold'>{d.short}</p>
+                  <p className='text-xs md:text-sm'>{d.month}</p>
                 </div>
               ))}
-              <button
-                onClick={handleNext}
-              >
-                 <Image src={'/icons/right-arrow-button.svg'} alt='button' width={200} height={20}/> 
+              <button onClick={handleNext}>
+                <Image src={'/icons/right-arrow-button.svg'} alt='button' width={200} height={20} />
               </button>
             </div>
           </div>
@@ -119,7 +126,7 @@ const Inspection = () => {
           {/* Time */}
           <div>
             <h2 className='font-semibold text-center text-lg md:text-2xl mb-4'>Time</h2>
-            <div className='flex gap-4'>
+            <div className='flex gap-4 overflow-x-auto scrollbar-hide'>
               {timeSlots.map((slot) => (
                 <button
                   key={slot.label}
@@ -130,15 +137,20 @@ const Inspection = () => {
                       : 'border-gray-200 text-gray-600'
                   }`}
                 >
-                  <p className='font-semibold'>{slot.label}</p>
-                  <p className='text-sm'>{slot.time}</p>
+                  <p className='text-xs md:text-sm font-semibold'>{slot.label}</p>
+                  <p className='text-xs md:text-sm'>{slot.time}</p>
                 </button>
               ))}
             </div>
           </div>
 
           <div className='w-full bg-gradient-to-r from-[#5D14AD] to-[#9747FF] p-[2px] rounded-lg'>
-            <BookInspectionModal open={showModal} setOpen={setShowModal} />
+            <BookInspectionModal
+              open={showModal}
+              setOpen={setShowModal}
+              selectedDate={selectedDate}
+              selectedTime={selectedTimeSlot}
+            />
           </div>
         </div>
       </div>
@@ -151,9 +163,10 @@ const Inspection = () => {
         inspectionTime={selectedTimeSlot}
         fee='₦5,000'
       />
-
     </div>
   )
 }
 
 export default Inspection
+
+
